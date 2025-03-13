6311d7fccbf5b3210e252d8ae47f976d6365b46d
Juanma Barea <juanmabareamartinez@gmail.com>
2025-03-13 19:36:15 +0100

    Fixe next config
    Signed-off-by: Juanma Barea <juanmabareamartinez@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    output: 'export',
-};
-
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-assetPrefix: isProd ? '/portfolio/' : '',
-basePath: isProd ? '/portfolio' : '',
-images: {
-    unoptimized: true,
-},
+const nextConfig: NextConfig = {
+    output: 'export',
+    assetPrefix: isProd ? '/portfolio/' : '',
+    basePath: isProd ? '/portfolio' : '',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
