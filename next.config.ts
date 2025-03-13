ad7560745c99027a439f2ea542cb6468eba87cc6
Juanma Barea <juanmabareamartinez@gmail.com>
2025-03-13 19:30:34 +0100

    Fixe problem with styles loading
    Signed-off-by: Juanma Barea <juanmabareamartinez@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,14 @@ const nextConfig: NextConfig = {
     output: 'export',
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+assetPrefix: isProd ? '/portfolio/' : '',
+basePath: isProd ? '/portfolio' : '',
+images: {
+    unoptimized: true,
+},
+};
+
 export default nextConfig;
